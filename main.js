fdab2d6c6375d197df0ac9f27a142e1beac9fa77
mariane101 <marianembaye@esp.sn>
2025-04-21 14:51:58 +0000

    lab9

--- main.js
+++ main.js
@@ -1,19 +1,25 @@
 const express = require("express");
 const layouts = require("express-ejs-layouts");
 const mongoose = require("mongoose");
+const methodOverride = require("method-override");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const flash = require("connect-flash");
+const passport = require("passport");
 
 const homeController = require("./controllers/homeController");
 const errorController = require("./controllers/errorController");
 const subscribersController = require("./controllers/subscribersController");
-
-// Ajoutez les contrôleurs
 const usersController = require("./controllers/usersController");
 const coursesController = require("./controllers/coursesController");
 
 const app = express();
 
 // Connexion à MongoDB
-mongoose.connect("mongodb://localhost:27017/ai_academy");
+mongoose.connect("mongodb://localhost:27017/ai_academy", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 const db = mongoose.connection;
 db.once("open", () => {
@@ -22,40 +28,43 @@ db.once("open", () => {
 db.on("error", err => {
   console.error("Erreur de connexion à MongoDB:", err);
 });
-
-// Ajouter le middleware method-override
-const methodOverride = require("method-override");
-app.use(methodOverride("_method", {
-methods: ["POST", "GET"]
-
-}));
-app.use(express.urlencoded({ extended: false }));
-// Routes pour les utilisateurs
-app.get("/users", usersController.index, usersController.indexView);
-app.get("/users/new", usersController.new);
-app.post("/users/create", usersController.create, usersController.redirectView);
-app.get("/users/:id", usersController.show, usersController.showView);
-app.get("/users/:id/edit", usersController.edit);
-app.put("/users/:id/update", usersController.update, usersController.redirectView);
-app.delete("/users/:id/delete", usersController.delete, usersController.redirectView);
-// Routes pour les cours
-app.get("/courses", coursesController.index, coursesController.indexView);
-app.get("/courses/new", coursesController.new);
-app.post("/courses/create", coursesController.create, coursesController.redirectView);
-app.get("/courses/:id", coursesController.show, coursesController.showView);
-app.get("/courses/:id/edit", coursesController.edit);
-app.put("/courses/:id/update", coursesController.update, coursesController.redirectView);
-app.delete("/courses/:id/delete", coursesController.delete, coursesController.redirectView);
-
+const authController = require("./controllers/authController");
 // Configuration serveur
 app.set("port", process.env.PORT || 3000);
 app.set("view engine", "ejs");
 
-// Middlewares
+// Middlewares de base
 app.use(layouts);
 app.use(express.json());
-
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"));
+app.use(methodOverride("_method", { methods: ["POST", "GET"] }));
+
+// Sessions, Cookies, Flash, Passport
+app.use(cookieParser("secret_passcode"));
+app.use(session({
+  secret: "secret_passcode",
+  cookie: { maxAge: 4000000 },
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Config Passport avec le modèle User
+const User = require("./models/user");
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// Variables disponibles dans toutes les vues
+app.use((req, res, next) => {
+  res.locals.flashMessages = req.flash();
+  res.locals.loggedIn = req.isAuthenticated();
+  res.locals.currentUser = req.user;
+  next();
+});
 
 // Routes principales
 app.get("/", homeController.index);
@@ -69,6 +78,35 @@ app.get("/subscribers", subscribersController.getAllSubscribers);
 app.get("/subscribers/new", subscribersController.getSubscriptionPage);
 app.post("/subscribers/create", subscribersController.saveSubscriber);
 app.get("/subscribers/:id", subscribersController.show);
+
+// Routes utilisateurs
+app.get("/users", usersController.index, usersController.indexView);
+app.get("/users/new", usersController.new);
+app.post("/users/create", usersController.create, usersController.redirectView);
+app.get("/users/:id", usersController.show, usersController.showView);
+app.get("/users/:id/edit", usersController.edit);
+app.put("/users/:id/update", usersController.update, usersController.redirectView);
+app.delete("/users/:id/delete", usersController.delete, usersController.redirectView);
+
+// Routes cours
+app.get("/courses", coursesController.index, coursesController.indexView);
+app.get("/courses/new", coursesController.new);
+app.post("/courses/create", coursesController.create, coursesController.redirectView);
+app.get("/courses/:id", coursesController.show, coursesController.showView);
+app.get("/courses/:id/edit", coursesController.edit);
+app.put("/courses/:id/update", coursesController.update, coursesController.redirectView);
+app.delete("/courses/:id/delete", coursesController.delete, coursesController.redirectView);
+
+// Routes d'authentification
+app.get("/login", authController.login);
+app.post("/login", authController.authenticate);
+app.get("/logout", authController.logout, usersController.redirectView);
+app.get("/signup", authController.signup);
+app.post("/signup", authController.register, usersController.redirectView);
+// Routes protégées - accessibles uniquement aux utilisateurs connectés
+app.use("/users", authController.ensureLoggedIn);
+app.use("/courses/new", authController.ensureLoggedIn);
+app.use("/courses/:id/edit", authController.ensureLoggedIn);
 
 // Gestion des erreurs
 app.use(errorController.pageNotFoundError);
--- models/user.js
+++ models/user.js
@@ -1,58 +1,64 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
+const passportLocalMongoose = require("passport-local-mongoose");
+
 const userSchema = new Schema(
-{
-name: {
-first: {
-type: String,
-trim: true
-},
-last: {
-type: String,
-trim: true
-}
-},
-email: {
-type: String,
-required: true,
-lowercase: true,
-unique: true
-},
-zipCode: {
-type: Number,
-min: [10000, "Code postal trop court"],
-max: 99999
-},
-password: {
-type: String,
-required: true
-},
-courses: [{ type: Schema.Types.ObjectId, ref: "Course" }],
-subscribedAccount: { type: Schema.Types.ObjectId, ref: "Subscriber" }
-},
-{
-timestamps: true
-}
+  {
+    name: {
+      first: {
+        type: String,
+        trim: true
+      },
+      last: {
+        type: String,
+        trim: true
+      }
+    },
+    email: {
+      type: String,
+      required: true,
+      lowercase: true,
+      unique: true
+    },
+    zipCode: {
+      type: Number,
+      min: [10000, "Code postal trop court"],
+      max: 99999
+    },
+    courses: [{ type: Schema.Types.ObjectId, ref: "Course" }],
+    subscribedAccount: { type: Schema.Types.ObjectId, ref: "Subscriber" }
+  },
+  {
+    timestamps: true
+  }
 );
+
 // Attribut virtuel pour le nom complet
-userSchema.virtual("fullName").get(function() {
-return `${this.name.first} ${this.name.last}`;
+userSchema.virtual("fullName").get(function () {
+  return `${this.name.first} ${this.name.last}`;
 });
+
 // Hook pre-save pour associer un abonné à l'utilisateur si les emails correspondent
-userSchema.pre("save", function(next) {
-let user = this;
-if (user.subscribedAccount === undefined) {
-mongoose.model("Subscriber").findOne({ email: user.email })
-.then(subscriber => {
-user.subscribedAccount = subscriber;
-next();
-})
-.catch(error => {
-console.log(`Erreur lors de la connexion avec l'abonné: ${error.message}`);
-next(error);
+userSchema.pre("save", function (next) {
+  let user = this;
+  if (user.subscribedAccount === undefined) {
+    mongoose.model("Subscriber").findOne({ email: user.email })
+      .then(subscriber => {
+        user.subscribedAccount = subscriber;
+        next();
+      })
+      .catch(error => {
+        console.log(`Erreur lors de la connexion avec l'abonné: ${error.message}`);
+        next(error);
+      });
+  } else {
+    next();
+  }
 });
-} else {
-next();
-}
+
+// Plugin passport-local-mongoose pour gérer mot de passe et authentification
+userSchema.plugin(passportLocalMongoose, {
+  usernameField: "email" // Utiliser email comme identifiant au lieu de username
 });
+
 module.exports = mongoose.model("User", userSchema);
