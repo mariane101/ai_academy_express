07704a1aaabb6214041bf2074fc114c2c07ae577
mariane101 <marianembaye@esp.sn>
2025-04-29 11:05:23 +0000

    lab9

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,119 +1,44 @@
-const User = require("../models/user");
+const jsonWebToken = require("jsonwebtoken"); // Assure-toi d'importer jwt
+const token_key = "TA_CLE_SECRETE"; // ou utilise process.env.TOKEN_KEY
 
-// Fonction utilitaire pour extraire les paramètres utilisateur du corps de la requête
-const getUserParams = (body = {}) => {
-  return {
-    name: {
-      first: body.first || "",
-      last: body.last || ""
-    },
-    email: body.email || "",
-    password: body.password || "",
-    zipCode: body.zipCode || ""
-  };
-};
+
 
 module.exports = {
-  index: (req, res, next) => {
-    User.find({})
-      .then(users => {
-        res.locals.users = users;
-        next();
-      })
-      .catch(error => {
-        console.log(`Erreur lors de la récupération des utilisateurs: ${error.message}`);
-        next(error);
+  index: (req, res, next) => { ... },
+
+  indexView: (req, res) => { ... },
+
+  new: (req, res) => { ... },
+
+  create: (req, res, next) => { ... },
+
+  redirectView: (req, res, next) => { ... },
+
+  show: (req, res, next) => { ... },
+
+  showView: (req, res) => { ... },
+
+  edit: (req, res, next) => { ... },
+
+  update: (req, res, next) => { ... },
+
+  delete: (req, res, next) => { ... },
+
+  getApiToken: (req, res) => {
+    if (req.user) {
+      let signedToken = jsonWebToken.sign(
+        {
+          data: req.user._id,
+          exp: Math.floor(Date.now() / 1000) + (60 * 60 * 24 * 30) // 30 jours
+        },
+        token_key
+      );
+      res.render("users/api-token", {
+        token: signedToken
       });
-  },
-
-  indexView: (req, res) => {
-    res.render("users/index");
-  },
-
-  new: (req, res) => {
-    res.render("users/new");
-  },
-
-  create: (req, res, next) => {
-    console.log("Données reçues :", req.body); // Pour vérifier ce qui est envoyé
-    let userParams = getUserParams(req.body);
-    User.create(userParams)
-      .then(user => {
-        res.locals.redirect = "/users";
-        res.locals.user = user;
-        next();
-      })
-      .catch(error => {
-        console.log(`Erreur lors de la création de l'utilisateur: ${error.message}`);
-        res.locals.redirect = "/users/new";
-        next();
-      });
-  },
-
-  redirectView: (req, res, next) => {
-    let redirectPath = res.locals.redirect;
-    if (redirectPath) res.redirect(redirectPath);
-    else next();
-  },
-
-  show: (req, res, next) => {
-    let userId = req.params.id;
-    User.findById(userId)
-      .then(user => {
-        res.locals.user = user;
-        next();
-      })
-      .catch(error => {
-        console.log(`Erreur lors de la récupération de l'utilisateur par ID: ${error.message}`);
-        next(error);
-      });
-  },
-
-  showView: (req, res) => {
-    res.render("users/show");
-  },
-
-  edit: (req, res, next) => {
-    let userId = req.params.id;
-    User.findById(userId)
-      .then(user => {
-        res.render("users/edit", {
-          user: user
-        });
-      })
-      .catch(error => {
-        console.log(`Erreur lors de la récupération de l'utilisateur par ID: ${error.message}`);
-        next(error);
-      });
-  },
-
-  update: (req, res, next) => {
-    let userId = req.params.id,
-      userParams = getUserParams(req.body);
-    User.findByIdAndUpdate(userId, {
-      $set: userParams
-    })
-      .then(user => {
-        res.locals.redirect = `/users/${userId}`;
-        res.locals.user = user;
-        next();
-      })
-      .catch(error => {
-        console.log(`Erreur lors de la mise à jour de l'utilisateur par ID: ${error.message}`);
-        next(error);
-      });
-  },
-
-  delete: (req, res, next) => {
-    let userId = req.params.id;
-    User.findByIdAndRemove(userId)
-      .then(() => {
-        res.locals.redirect = "/users";
-        next();
-      })
-      .catch(error => {
-        console.log(`Erreur lors de la suppression de l'utilisateur par ID: ${error.message}`);
-        next();
-      });
+    } else {
+      req.flash("error", "Vous devez être connecté pour obtenir un token API.");
+      res.redirect("/login");
+    }
   }
 };
--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Imports
 const express = require("express");
 const layouts = require("express-ejs-layouts");
 const mongoose = require("mongoose");
@@ -6,113 +7,60 @@ const cookieParser = require("cookie-parser");
 const session = require("express-session");
 const flash = require("connect-flash");
 const passport = require("passport");
-
-const homeController = require("./controllers/homeController");
-const errorController = require("./controllers/errorController");
-const subscribersController = require("./controllers/subscribersController");
-const usersController = require("./controllers/usersController");
-const coursesController = require("./controllers/coursesController");
-
-const app = express();
-
-// Connexion à MongoDB
-mongoose.connect("mongodb://localhost:27017/ai_academy", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
-
+const httpStatus = require("http-status-codes");
+const routes = require("./routes/index"); 
+// Configuration de la connexion à MongoDB
+mongoose.connect(
+"mongodb://localhost:27017/ai_academy",
+{ useNewUrlParser: true }
+);
 const db = mongoose.connection;
 db.once("open", () => {
-  console.log("Connexion réussie à MongoDB en utilisant Mongoose!");
+console.log("Connexion réussie à MongoDB en utilisant Mongoose!");
 });
-db.on("error", err => {
-  console.error("Erreur de connexion à MongoDB:", err);
-});
-const authController = require("./controllers/authController");
-// Configuration serveur
+// Initialisation de l'application Express
+const app = express();
+// Configuration de l'application
 app.set("port", process.env.PORT || 3000);
 app.set("view engine", "ejs");
-
-// Middlewares de base
-app.use(layouts);
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"));
-app.use(methodOverride("_method", { methods: ["POST", "GET"] }));
-
-// Sessions, Cookies, Flash, Passport
-app.use(cookieParser("secret_passcode"));
-app.use(session({
+app.use(layouts);
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(methodOverride("_method", {
+  methods: ["POST", "GET"]
+  }));
+  // Configuration des cookies et des sessions
+  app.use(cookieParser("secret_passcode"));
+  app.use(session({
   secret: "secret_passcode",
-  cookie: { maxAge: 4000000 },
+  cookie: {
+  maxAge: 4000000
+  },
   resave: false,
   saveUninitialized: false
-}));
-app.use(flash());
-app.use(passport.initialize());
-app.use(passport.session());
-
-// Config Passport avec le modèle User
-const User = require("./models/user");
-passport.use(User.createStrategy());
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
-
-// Variables disponibles dans toutes les vues
-app.use((req, res, next) => {
+  }));
+  // Configuration de flash messages
+  app.use(flash());
+  // Configuration de Passport
+  app.use(passport.initialize());
+  app.use(passport.session());
+  // Configuration du User model pour Passport
+  const User = require("./models/user");
+  passport.use(User.createStrategy());
+  passport.serializeUser(User.serializeUser());
+  passport.deserializeUser(User.deserializeUser());
+  // Middleware pour rendre les variables locales disponibles dans toutes les vues
+  app.use((req, res, next) => {
   res.locals.flashMessages = req.flash();
   res.locals.loggedIn = req.isAuthenticated();
   res.locals.currentUser = req.user;
   next();
-});
-
-// Routes principales
-app.get("/", homeController.index);
-app.get("/about", homeController.about);
-app.get("/courses", homeController.courses);
-app.get("/contact", homeController.contact);
-app.post("/contact", homeController.processContact);
-
-// Routes abonnés
-app.get("/subscribers", subscribersController.getAllSubscribers);
-app.get("/subscribers/new", subscribersController.getSubscriptionPage);
-app.post("/subscribers/create", subscribersController.saveSubscriber);
-app.get("/subscribers/:id", subscribersController.show);
-
-// Routes utilisateurs
-app.get("/users", usersController.index, usersController.indexView);
-app.get("/users/new", usersController.new);
-app.post("/users/create", usersController.create, usersController.redirectView);
-app.get("/users/:id", usersController.show, usersController.showView);
-app.get("/users/:id/edit", usersController.edit);
-app.put("/users/:id/update", usersController.update, usersController.redirectView);
-app.delete("/users/:id/delete", usersController.delete, usersController.redirectView);
-
-// Routes cours
-app.get("/courses", coursesController.index, coursesController.indexView);
-app.get("/courses/new", coursesController.new);
-app.post("/courses/create", coursesController.create, coursesController.redirectView);
-app.get("/courses/:id", coursesController.show, coursesController.showView);
-app.get("/courses/:id/edit", coursesController.edit);
-app.put("/courses/:id/update", coursesController.update, coursesController.redirectView);
-app.delete("/courses/:id/delete", coursesController.delete, coursesController.redirectView);
-
-// Routes d'authentification
-app.get("/login", authController.login);
-app.post("/login", authController.authenticate);
-app.get("/logout", authController.logout, usersController.redirectView);
-app.get("/signup", authController.signup);
-app.post("/signup", authController.register, usersController.redirectView);
-// Routes protégées - accessibles uniquement aux utilisateurs connectés
-app.use("/users", authController.ensureLoggedIn);
-app.use("/courses/new", authController.ensureLoggedIn);
-app.use("/courses/:id/edit", authController.ensureLoggedIn);
-
-// Gestion des erreurs
-app.use(errorController.pageNotFoundError);
-app.use(errorController.internalServerError);
-
-// Lancement du serveur
-app.listen(app.get("port"), () => {
-  console.log(`Le serveur a démarré sur http://localhost:${app.get("port")}`);
-});
+  });
+  // Utilisation des routes
+  app.use("/", routes);
+  // Démarrage du serveur
+  app.listen(app.get("port"), () => {
+  console.log(`Le serveur a démarré et écoute sur le port: ${app.get("port")}`);
+  console.log(`Serveur accessible à l'adresse: http://localhost:${app.get("port")}`);
+  });
